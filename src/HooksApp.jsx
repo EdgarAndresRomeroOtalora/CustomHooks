d0e5f41c5ad4ee4d6a837033f0504e48dfcb5b36
Edgar Andres Romero Otalora <andres.romero.otalora@gmail.com>
2024-08-26 16:39:50 -0500

    useMemo() Se agrega un hook para memorizar un calculo y solo hacerlo cuando se necesite

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -3,17 +3,14 @@
 import { FetchApp } from "./components/FetchApp"
 import { CounterApp } from "./components/CounterApp"
 import { FormsApp } from "./components/FormsApp"
+import { CalculoPesadoApp } from "./components/CalculoPesadoApp"
 
 export const HooksApp = () => {
   return (
     <>
       <h1>Aplicación de Hooks</h1>
       <hr />
-      <FetchApp />
-      <hr />
-      <CounterApp/>
-      <hr />
-      <FormsApp/>
+      <CalculoPesadoApp />
     </>
   )
 }
