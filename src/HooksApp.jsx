817f72af58989bc5ddf938dae21edb5581674c1d
Edgar Andres Romero Otalora <andres.romero.otalora@gmail.com>
2024-08-26 17:11:43 -0500

    useCallback() Se implementa un Hook de callback

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -4,13 +4,14 @@ import { FetchComponent } from "./components/FetchComponent"
 import { CounterComponent } from "./components/CounterComponent"
 import { FormsComponent } from "./components/FormsComponent"
 import { CalculoPesadoComponent } from "./components/CalculoPesadoComponent"
+import { CallBackComponent } from "./components/CallBackComponent"
 
 export const HooksApp = () => {
   return (
     <>
       <h1>Aplicación de Hooks</h1>
       <hr />
-      <CalculoPesadoComponent />
+      <CallBackComponent />
     </>
   )
 }
